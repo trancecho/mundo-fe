371b65296c6913da22d03d38afeca87e23a98b84
Anhofa <22050805@hdu.edu.cn>
2024-12-22 23:34:13 +0800

    小改
    Signed-off-by: Anhofa <22050805@hdu.edu.cn>

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,14 @@ import BindRegisterPage from '@/pages/Register/BindRegister'; // 注册
 import Verify from '@/pages/Register/Verify'; // 注册验证
 import DataStation from '@/pages/DataStation';//资料站
 import Forum from '@/pages/Forum';//论坛
-import QAndA from '@/pages/QAndA';//答疑
+import QAndA from '@/pages/QAndA/QAndA';//答疑
 import TeamUp from '@/pages/TeamUp';//组队
-
 import Houtai from './pages/Houtai/Houtai'; // 引入 Houtai.tsx
-import Post from './components/center/post';
-
+import Post from './components/center/post';//发布帖子
 import FrontPage from '@/pages/FrontPage.tsx';//首页
 import AnswerWindow from '@/components/CustomerService/AnswerWindow'; // 导入答案窗口组件
-import DetailMessage from './pages/QAndA/DetailMessage';
-
-import Review from '@/pages/Review/review'; // 审核
+import DetailMessage from './pages/QAndA/DetailMessage';//帖子详情页
+import Review from '@/pages/Review/Review'; // 审核
 
 
 import './App.css';
@@ -32,22 +29,21 @@ const App: React.FC = () => {
                     <AnswerWindow /> {/*客服组件*/}
                     <Routes>
                         <Route path="/" element={<FrontPage />} /> 
-                        {/* <Route path="/dashboard" element={<DashboardPage />} />  */}
 
-                        {/* <Route path="/" element={<Home />} /> */}
+                            <Route path="/houtai" element={<Houtai />} />
                             <Route path="/datastation" element={<DataStation />} />
                             <Route path="/forum" element={<Forum />} />
                             <Route path="/qanda" element={<QAndA />} />
                             <Route path="/qanda/detail" element={<DetailMessage />} />
                             <Route path="/center" element={<Post />}/>
-
                             <Route path="/teamup" element={<TeamUp />} />
                             <Route path="/login" element={<Login />} />
                             <Route path="/register" element={<RegisterPage />} />
                             <Route path="/bindregister" element={<BindRegisterPage />} />
                             <Route path="/register/verify/v2" element={<Verify />} />
+                            <Route path="/review" element={<Review />} />
 
-                        {/* 可以根据实际需求继续添加更多的路由配置 */}
+
                         </Routes>
                     </div>
             </Router>
