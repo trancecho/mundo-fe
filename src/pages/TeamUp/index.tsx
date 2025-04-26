1b5dec9bca93b9f5bbe7137999d316fc08b1eaed
qqx12345 <1244064269@qq.com>
2025-04-26 14:36:46 +0800

    删除加入

--- src/pages/TeamUp/index.tsx
+++ src/pages/TeamUp/index.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import Header from '@/components/ui/Header/Header.tsx'
 import style from "./teamup.module.css";
 import { useAuth } from '@/context/AuthContext';
-import { getteamup, apply } from "../../router/api";
+import { getteamup } from "../../router/api";
 import { SearchProvider, useSearch } from "@/components/ui/Header/SearchContext";
 
 type detail = {
@@ -17,19 +17,19 @@ type detail = {
 
 
 
-const Detail = ({ detail, jumpto, apply }: { detail: detail; jumpto: () => void; apply: () => Promise<any> }) => {
+const Detail = ({ detail, jumpto }: { detail: detail; jumpto: () => void; }) => {
   const ref = useRef<HTMLDivElement | null>(null);
-  const applyto = () => {
-    apply().then(res => {
-      if (res.data.code === 200) {
-        alert('申请成功！');
-        jumpto(); 
-      }
-    }).catch(err => {
-      alert(err.response.data.message);
-      jumpto();
-    });
-  };
+  // const applyto = () => {
+  //   apply().then(res => {
+  //     if (res.data.code === 200) {
+  //       alert('申请成功！');
+  //       jumpto(); 
+  //     }
+  //   }).catch(err => {
+  //     alert(err.response.data.message);
+  //     jumpto();
+  //   });
+  // };
   useEffect(() => {
     const handleClickOutside = (e: MouseEvent) => {
       if (ref.current && !ref.current.contains(e.target as Node)) {
@@ -64,9 +64,9 @@ const Detail = ({ detail, jumpto, apply }: { detail: detail; jumpto: () => void;
         </div>
         <div className={style.teamMeta}>
           <div>人数：{detail.Number}</div>
-          <button className={style.joinButton} onClick={applyto}>
+          {/* <button className={style.joinButton} onClick={applyto}>
             <span>加入</span>
-          </button>
+          </button> */}
         </div>
       </div>
     </div>
