aed8ad68cbfde352bc73fdb4c11e0bcbb07288ea
yuuki798 <b19357160114@gmail.com>
2025-06-08 13:22:41 +0800

    chore: 添加一个测试服

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,39 @@
-import {defineConfig, loadEnv} from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from "path"
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), "VITE_");
-    return{
-        plugins: [react()],
-        resolve: {
-            alias: {
-                "@": path.resolve(__dirname, "./src"),
-                "react-router-dom": path.resolve(__dirname, "node_modules/react-router-dom")
-            },
-        },
-        css: {
-            modules: {
-                scopeBehaviour: 'local',
-            },
-            devSourcemap: true,
-        },
-        build:{
-            sourcemap: true,
-        },
-        server: {
-            port:env.VITE_port,
-            // proxy: {
-            //     '/api': {
-            //         target: process.env.baseURL,
-            //         changeOrigin: true,
-            //         rewrite: (path) => path.replace(/^\/api/, ''),
-            //     },
-            // },
-        },
-        esbuild:{
-            jsxDev:mode!=='production',
-        }
+  const env = loadEnv(mode, process.cwd(), 'VITE_')
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        'react-router-dom': path.resolve(__dirname, 'node_modules/react-router-dom')
+      }
+    },
+    css: {
+      modules: {
+        scopeBehaviour: 'local'
+      },
+      devSourcemap: true
+    },
+    build: {
+      sourcemap: true
+    },
+    server: {
+      port: env.VITE_port
+      // proxy: {
+      //     '/api': {
+      //         target: process.env.baseURL,
+      //         changeOrigin: true,
+      //         rewrite: (path) => path.replace(/^\/api/, ''),
+      //     },
+      // },
+    },
+    esbuild: {
+      jsxDev: mode === 'development' || mode === 'test'
     }
+  }
 })
